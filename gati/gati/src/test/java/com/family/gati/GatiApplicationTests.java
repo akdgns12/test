5be8903869713f2caffeaf397a1bed5bf94f8acb
akdgns12 <akdgns12@naver.com>
2023-01-20 22:17:40 +0900

    ignore test

---
+++ gati/gati/src/test/java/com/family/gati/GatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.family.gati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
